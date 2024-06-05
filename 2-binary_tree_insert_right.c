8b69843bdcd97cfeb4e8267a1ded2ff46c4560e6
DrissArezki <driss.arezki5@gmail.com>
2024-06-05 00:47:22 -0700

    binary_trees

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - add a node to the right of the parent node
+ * @parent: parent
+ * @value: value of the node to add
+ * Return: new node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+        binary_tree_t *new;
+
+        if (parent == NULL)
+        {
+                return (NULL);
+        }
+
+        new = binary_tree_node(parent, value);
+        if (new == NULL)
+        {
+                return (NULL);
+        }
+        if (parent->right != NULL)
+        {
+                new->right = parent->right;
+                parent->right->parent = new;
+        }
+        parent->right = new;
+        return (new);
+}
